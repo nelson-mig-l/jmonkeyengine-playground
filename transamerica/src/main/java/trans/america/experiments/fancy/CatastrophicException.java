6fb71ff58e866954f9b97b1341b49e51f84a3fd4
nelson-mig-l <nelson.mig.l@gmail.com>
2022-01-26 20:54:34 +0100

    move transamerica into separate projetc

--- transamerica/src/main/java/trans/america/experiments/fancy/CatastrophicException.java
+++
@@ -1,9 +0,0 @@
-package trans.america.experiments.fancy;
-
-public class CatastrophicException extends RuntimeException {
-
-    public CatastrophicException(String message) {
-        super(message);
-    }
-
-}
--- transamerica/src/main/java/trans/america/experiments/fancy/car/FancySuspensionDefinition.java
+++
@@ -1,7 +0,0 @@
-package trans.america.experiments.fancy.car;
-
-public class FancySuspensionDefinition extends CustomSuspensionDefinition {
-    public FancySuspensionDefinition() {
-        super(120.0f, 0.2f, 0.3f, 10000);
-    }
-}
